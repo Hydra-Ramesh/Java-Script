044c9d895a9a3c3b047859c79a0613377c97ba73
Ramesh Das <157310459+Hydra-Ramesh@users.noreply.github.com>
2024-02-23 22:40:04 +0530

    Object type of data

---
+++ Object.js
@@ -0,0 +1,11 @@
+/**
+ * It is contain more key and value.
+ * It also define by the user.
+ */
+var person={
+    name: "Ramesh",
+     age: 19,
+     hobby: "Engineer"
+}
+console.log(person)
+console.log(typeof person)
