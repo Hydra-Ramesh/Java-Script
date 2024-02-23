48f98b339cb5c0f41cdcc591cab39f352ba5fb84
Ramesh Das <157310459+Hydra-Ramesh@users.noreply.github.com>
2024-02-23 22:38:04 +0530

    Big Integer

---
+++ BigInt.js
@@ -0,0 +1,8 @@
+/**
+ * It is used for assing a big value.
+ * Like million , billion, trilion.
+ * Use case of this data type is in the Data Base system.
+ */
+
+var big_num=124n
+console.log(typeof big_num)
