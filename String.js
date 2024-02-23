d3e3b570ab9affb2f661d35925b97238611dd68f
Ramesh Das <157310459+Hydra-Ramesh@users.noreply.github.com>
2024-02-23 22:40:22 +0530

    String type data

---
+++ String.js
@@ -0,0 +1,9 @@
+/**
+ * Sting is a primitive data type.
+ * It is a collection of more than one letter.
+ */
+var name= "Ramesh"
+var title= 'String'
+console.log(name)
+console.log(typeof name)
+console.log(typeof title)
