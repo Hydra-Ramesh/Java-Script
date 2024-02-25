6c704966854251096fcbd55203ca8f761d354fbb
Ramesh Das <157310459+Hydra-Ramesh@users.noreply.github.com>
2024-02-25 23:37:16 +0530

    Arithmetic Operator

---
+++ Arithmath.js
@@ -0,0 +1,18 @@
+/**
+ * Arithmath operator is used to do 
+ */
+
+console.log(4+5)
+console.log(10-3)
+console.log(23*2)
+console.log(20/4)
+console.log(25%5)
+
+var i=1
+i=i+1
+console.log(i)
+
+var i=4
+console.log(i++)//Post Increment
+console.log(i)
+console.log(++i)//Pre Increment
