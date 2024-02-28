adaa424bcc717a3a0b897d98254d8ac33ec3a972
Ramesh Das <157310459+Hydra-Ramesh@users.noreply.github.com>
2024-02-28 21:40:52 +0530

    Concatenation

---
+++ Concatination.js
@@ -0,0 +1,10 @@
+/**
+ * Concatination is used for concat two or more than two String.
+ */
+let first="Ramesh"
+let second=" Das"
+
+let full_name=first.concat(second)
+console.log(full_name)
+
+console.log(first+second)
