2045b2324cc19c6edc44806c26b148ddc3cacaf4
Ramesh Das <157310459+Hydra-Ramesh@users.noreply.github.com>
2024-02-28 21:40:26 +0530

    Casing Operation

---
+++ Casing.js
@@ -0,0 +1,9 @@
+/**
+ * Casing actually use for Convert a String to Upper Case or Lower Case.
+ */
+let first="Ramesh"
+let second=" Das"
+
+let full_name=first.concat(second)
+console.log(full_name)
+console.log(full_name.toUpperCase())
