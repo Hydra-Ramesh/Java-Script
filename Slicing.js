f0fb185683d88f0849660aecb1b5cfc631a85190
Ramesh Das <157310459+Hydra-Ramesh@users.noreply.github.com>
2024-02-28 21:42:01 +0530

    Slicing a string

---
+++ Slicing.js
@@ -0,0 +1,14 @@
+/**
+ * Slicing is actually used to get a part of a string.
+ */
+
+let first="Ramesh"
+let second=" Das"
+
+let full_name=first.concat(second)
+console.log(full_name)
+
+console.log(full_name.slice(2))
+console.log(full_name.slice(2,5))
+console.log(full_name.slice(-5))
+console.log(full_name.slice(-5,-1))
