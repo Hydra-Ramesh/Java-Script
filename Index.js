ba55a713a6c47fca8fe34ef257401ff8e8dda034
Ramesh Das <157310459+Hydra-Ramesh@users.noreply.github.com>
2024-02-28 21:41:15 +0530

    Index find operation

---
+++ Index.js
@@ -0,0 +1,11 @@
+/**
+ * It is actually a method of reverse indexing.
+ * Here we have to print the key or index of a letter including in a string.
+ */
+
+let first="Ramesh"
+let second=" Das"
+
+let full_name=first.concat(second)
+console.log(full_name)
+console.log(full_name.indexOf('a'))
